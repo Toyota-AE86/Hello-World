86c3961f71f264f12895ae6a9de8f123f037f3d2
Toyota-AE86 <32719262+Toyota-AE86@users.noreply.github.com>
2017-10-14 22:42:04 +0100

    idek

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
+<script>
+function colorchange(id)
+{
 
+  var background = document.getElementById(id).style.background;
+
+  if(background = "rgb(255,145,0)")
+  {
+  document.getElementById(id).style.background = "rgb(26,255,0)";
+  }
+  if(background == "rgb(26,255,0)")
+  {
+    document.getElementById(id).style.background = "rgb(255,145,0)";
+  }
+
+}
+</script>
