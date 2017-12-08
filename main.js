7f1d7a5cccde4048cb60f9e8295dac17820711a6
Toyota-AE86 <32719262+Toyota-AE86@users.noreply.github.com>
2017-12-08 20:25:16 +0000

    vars and func

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
+var varNumBasicCar = 0;
+var varNumMuscleCar = 0;
+var Money = 0;
+
 
 document.getElementById("btnBuyBasicCar").addEventListener("click", funcBuyBasicCar);
 
 document.getElementById("btnBuyMuscleCar").addEventListener("click", funcBuyMuscleCar);
 
-
+function funcBuyBasicCar() {
+    
+}
