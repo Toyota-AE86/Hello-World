bcee841c1d278766c6ee741170314ca1a6be0a7a
Toyota-AE86 <32719262+Toyota-AE86@users.noreply.github.com>
2017-12-08 00:21:33 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,1 @@
-<script>
-function colorchange(id)
-{
 
-  var background = document.getElementById(id).style.background;
-
-  if(background = "rgb(255,145,0)")
-  {
-  document.getElementById(id).style.background = "rgb(26,255,0)";
-  }
-  if(background == "rgb(26,255,0)")
-  {
-    document.getElementById(id).style.background = "rgb(255,145,0)";
-  }
-  document.getElementByID("buttonID").addEventListener("click", function);
-
-
-}
-</script>
