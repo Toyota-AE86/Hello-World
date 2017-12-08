6cdc4473222263e70446c3a69ab8fa8e8c3eaab0
Toyota-AE86 <32719262+Toyota-AE86@users.noreply.github.com>
2017-12-08 21:30:25 +0000

    if statements

--- main.js
+++ main.js
@@ -8,15 +8,19 @@ document.getElementById("btnBuyBasicCar").addEventListener("click", funcBuyBasic
 document.getElementById("btnBuyMuscleCar").addEventListener("click", funcBuyMuscleCar);
 
 function funcBuyBasicCar() {
+    if(varMoney>=10){    
     varNumBasicCar++
     varMoney-=10
     document.getElementById("badgeBasicCar").innerHTML = varNumBasicCar;
     document.getElementById("spanBankAccount").innerHTML = varMoney;
+    }
 }
 
 function funcBuyMuscleCar() {
+    if(varMoney>=100){  
     varNumMuscleCar++
     varMoney-=100
     document.getElementById("badgeMuscleCar").innerHTML = varNumMuscleCar;
     document.getElementById("spanBankAccount").innerHTML = varMoney;
+    }
 }
